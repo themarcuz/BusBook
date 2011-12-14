928b24de6185962e56c04774e25ebf3013963fd1
Marco Lunazzi <themarcuz@gmail.com>
2011-12-15 00:44:30 +0100

    Lavoro sulla lista dei viaggi

---
+++ Xlns.BusBook.UI.Web/Content/scripts/viaggio.lista.js
@@ -0,0 +1,13 @@
+﻿$(function () {
+    $(".tileElement").click(function () { alert("click sul viaggio"); });
+    $(".hiddenTappaDetail").hide();
+    $(".visualizzaDettaglioTappaLink")
+            .each(function () {
+                var target = $(this);
+                var id = target.attr('data-viaggio-id');
+                target.click(function () {
+                    $("#hiddenTappaDetail" + id).toggle("slide");
+                    return false;
+                });
+            });
+});
