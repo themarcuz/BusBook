bab1d888653cc617b2d4c0fa89127001ec6e3974
Marco Lunazzi <themarcuz@gmail.com>
2012-02-19 17:23:55 +0100

    Aggiunto campo per numero partecipanti massimo per il viaggio (posti in bus)
    Signed-off-by: Marco Lunazzi <themarcuz@gmail.com>

--- Xlns.BusBook.Core/Model/Viaggio.cs
+++ Xlns.BusBook.Core/Model/Viaggio.cs
@@ -37,6 +37,11 @@ namespace Xlns.BusBook.Core.Model
 
         public virtual int DistanzaPercorsa { get; set; }
 
+        [Display(Name = "Capienza del bus")]
+        [Required(ErrorMessage="Informazione obbligatoria")]        
+        public virtual int TotalePosti { get; set; }
+
+
         public override string ToString()
         {
             return String.Format("{0} - {1}", Id, Nome);
