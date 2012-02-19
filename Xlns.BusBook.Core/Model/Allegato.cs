1e9dd669bb4aed4d8ebcaced2eae34fc49b52e2c
Marco Lunazzi <themarcuz@gmail.com>
2012-02-19 20:45:20 +0100

    Iniziata la gestione dell'upload dei depliant
    Signed-off-by: Marco Lunazzi <themarcuz@gmail.com>

---
+++ Xlns.BusBook.Core/Model/Allegato.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Xlns.BusBook.Core.Model
+{
+    public class Allegato : ModelEntity
+    {
+        public virtual String NomeFile { get; set; }
+        public virtual byte[] RawFile { get; set; }
+    }
+   
+}
