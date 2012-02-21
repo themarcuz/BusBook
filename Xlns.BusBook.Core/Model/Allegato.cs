252048645197790b835c0d352c2a632473da80f4
Marco Lunazzi <themarcuz@gmail.com>
2012-02-21 10:09:49 +0100

    Aggiustato l'ultimo merge
    Signed-off-by: Marco Lunazzi <themarcuz@gmail.com>

--- Xlns.BusBook.Core/Model/Allegato.cs
+++ Xlns.BusBook.Core/Model/Allegato.cs
@@ -5,22 +5,16 @@ using System.Text;
 
 namespace Xlns.BusBook.Core.Model
 {
-<<<<<<< HEAD
+
     public abstract class Allegato : ModelEntity
-=======
-    public class Allegato : ModelEntity
->>>>>>> 1e9dd669bb4aed4d8ebcaced2eae34fc49b52e2c
     {
         public virtual String NomeFile { get; set; }
         public virtual byte[] RawFile { get; set; }
     }
-<<<<<<< HEAD
 
     public class AllegatoViaggio : Allegato
     {
         public virtual Viaggio Viaggio { get; set; }
     }
-=======
->>>>>>> 1e9dd669bb4aed4d8ebcaced2eae34fc49b52e2c
    
 }
--- Xlns.BusBook.UI.Web/Controllers/AllegatoController.cs
+++ Xlns.BusBook.UI.Web/Controllers/AllegatoController.cs
@@ -11,10 +11,8 @@ namespace Xlns.BusBook.UI.Web.Controllers
 {
     public class AllegatoController : Controller
     {
-<<<<<<< HEAD
+
         private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
-=======
->>>>>>> 1e9dd669bb4aed4d8ebcaced2eae34fc49b52e2c
 
         /// <summary>
         /// 
@@ -23,7 +21,6 @@ namespace Xlns.BusBook.UI.Web.Controllers
         /// <param name="tipo">1: depliant, 2: immagine promozionale</param>
         /// <returns></returns>
         [ChildActionOnly]
-<<<<<<< HEAD
         public ActionResult New(Viaggio viaggio, int tipo)
         {
             var avm = new AllegatoViewModel() { Viaggio = viaggio, Tipo = (AllegatoViewModel.TipoAllegato)tipo };
@@ -37,18 +34,10 @@ namespace Xlns.BusBook.UI.Web.Controllers
         /// <param name="tipo">1: depliant, 2: immagine promozionale</param>
         /// <returns></returns>        
         public ActionResult Edit(Viaggio viaggio)
-        {            
+        {
             return PartialView(viaggio);
         }
 
-=======
-        public ActionResult New(int idViaggio, int tipo)
-        {
-            var avm = new AllegatoViewModel() { IdViaggio = idViaggio, Tipo = (AllegatoViewModel.TipoAllegato)tipo };
-            return PartialView(avm);
-        }
-
->>>>>>> 1e9dd669bb4aed4d8ebcaced2eae34fc49b52e2c
         [ChildActionOnly]
         public ActionResult Show(int id)
         {
@@ -58,20 +47,15 @@ namespace Xlns.BusBook.UI.Web.Controllers
 
         public ActionResult Upload(int idViaggio, int tipo)
         {
-<<<<<<< HEAD
             var avm = new AllegatoViewModel() { Viaggio = new Viaggio() { Id = idViaggio }, Tipo = (AllegatoViewModel.TipoAllegato)tipo };
-=======
-            var avm = new AllegatoViewModel() { IdViaggio = idViaggio, Tipo = (AllegatoViewModel.TipoAllegato)tipo };
->>>>>>> 1e9dd669bb4aed4d8ebcaced2eae34fc49b52e2c
             return PartialView(avm);
         }
 
         [HttpPost]
-<<<<<<< HEAD
         public ActionResult Save(AllegatoViewModel avm, HttpPostedFileBase file)
         {
             try
-            {                
+            {
                 var vr = new ViaggioRepository();
                 var idViaggio = avm.Viaggio.Id;
                 var viaggio = vr.GetById(idViaggio);
@@ -89,36 +73,15 @@ namespace Xlns.BusBook.UI.Web.Controllers
                 viaggio.Depliant = allegato;
 
                 vr.Save(viaggio);
-                return RedirectToAction("Edit", new { allegato = allegato});
+                return RedirectToAction("Edit", new { allegato = allegato });
             }
-            catch (Exception ex) 
+            catch (Exception ex)
             {
                 String msg = "Impossibile caricare l'allegato";
                 avm.Errore = msg;
                 logger.ErrorException(msg, ex);
                 return PartialView("Upload", avm);
             }
-            
-=======
-        public ActionResult Save(int idViaggio, int tipo, HttpPostedFileBase file)
-        {
-            var avm = new AllegatoViewModel() { IdViaggio = idViaggio, Tipo = (AllegatoViewModel.TipoAllegato)tipo };
-            var vr = new ViaggioRepository();
-            var viaggio = vr.GetById(idViaggio);
-            Int32 length = file.ContentLength;
-            byte[] rawFile = new byte[length];
-            file.InputStream.Read(rawFile, 0, length);
-            var allegato = new Allegato()
-            {
-                RawFile = rawFile,
-                NomeFile = file.FileName
-            };
-            viaggio.Depliant = allegato;
-
-            vr.Save(viaggio);
-
-            return PartialView(avm);
->>>>>>> 1e9dd669bb4aed4d8ebcaced2eae34fc49b52e2c
         }
 
     }
