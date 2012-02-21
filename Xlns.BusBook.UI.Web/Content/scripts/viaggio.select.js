3f8098ac23e6a76c564857c61597d66b18f5c498
Lorenzo Cavaciocchi <cava_l@hotmail.com>
2012-02-22 00:19:11 +0100

    prima versione funzionante della composizione flyer

---
+++ Xlns.BusBook.UI.Web/Content/scripts/viaggio.select.js
@@ -0,0 +1,18 @@
+﻿/// <reference path="../../Scripts/jquery-1.5.1-vsdoc.js" />
+$('input[type=checkbox]').click(function (e) {
+    var errMsg = "Errore: aggiornamento flyer fallito";
+
+    if ($(this).is(':checked'))
+        errMsg = "Impossibile aggiungere il viaggio al flyer";
+    else
+        errMsg = "Impossibile rimuovere il viaggio dal flyer";
+
+    $.ajax({
+        type: 'POST',
+        url: "/Flyer/ToggleViaggio",
+        cache: false,
+        data: { idFlyer: $('#idFlyer')[0].value, idViaggio: this.id },
+        context: $(this),
+        error: function () { alert(errMsg); }
+    });
+});
