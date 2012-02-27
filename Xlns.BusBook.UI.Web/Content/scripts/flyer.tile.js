22fffa1a5f9401a04337e7ee9c745436fbed7d6d
Lorenzo Cavaciocchi <cava_l@hotmail.com>
2012-02-27 22:05:14 +0100

    aggiunta conferma cancellazione.Ma alla cancellazione di un flyer dalla lista che mostra gli ultimi 5, bisognerebbe far apparire quello che prima era il sesto...

--- Xlns.BusBook.UI.Web/Content/scripts/flyer.tile.js
+++ Xlns.BusBook.UI.Web/Content/scripts/flyer.tile.js
@@ -4,15 +4,33 @@ function Tile(IdFlyer) {
     $('#TileDivFlyer' + IdFlyer).find('[data-delete-command]').each(function () {
         var target = $(this);
         target.click(function (e) {
-            $.ajax({
-                url: "/Flyer/DeleteAjax/" + IdFlyer,
-                cache: false,
-                context: target,
-                success: function (data) { $(this).parents('.tileElement').hide('slow'); },
-                error: function () { alert("Impossibile eliminare il flyer"); }
+            $("#dialog-confirm").dialog({
+                resizable: false,
+                height: 140,
+                modal: true,
+                buttons: {
+                    "Continua": function () {
+                        $(this).dialog("close");
+                        Delete(IdFlyer, target);
+                    },
+                    "Annulla": function () {
+                        $(this).dialog("close");
+                    }
+                }
             });
+
             return false;
         });
-    });    
+    });
+}
+
+function Delete(IdFlyer, target) {
+    $.ajax({
+        url: "/Flyer/DeleteAjax/" + IdFlyer,
+        cache: false,
+        context: target,
+        success: function (data) { $(this).parents('.tileElement').hide('slow'); },
+        error: function () { alert("Impossibile eliminare il flyer"); }
+    });
 }
     
