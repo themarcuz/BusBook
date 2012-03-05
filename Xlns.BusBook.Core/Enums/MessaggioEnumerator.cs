97e798c1817f708c27e9808ba5ca5173d762ffed
Alex Jay <alex.jandiri@gmail.com>
2012-03-05 02:48:10 +0100

    Gestione Messaggi; Aggiunto link alla privacy; modifiche varie

---
+++ Xlns.BusBook.Core/Enums/MessaggioEnumerator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Xlns.BusBook.Core.Enums
+{
+    public enum MessaggioEnumerator
+    {
+        NonLetto = 0x0,
+        Letto = 0x1,
+        Cancellato = 0x2,
+    }
+}
---
+++ Xlns.BusBook.UI.Web/Models/MenuView.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Xlns.BusBook.Core.Model;
+using Xlns.BusBook.Core.Repository;
+
+namespace Xlns.BusBook.UI.Web.Models
+{
+    public class MenuView
+    {
+        public Utente Utente { get; set; }
+        public IList<Messaggio> Messaggi { get; set; }
+
+        public MenuView(Utente utente)
+        {
+            Utente = utente;
+            var mr = new MessaggioRepository();
+            Messaggi = mr.GetMessaggiUnreadByDestinatario(utente.Id);
+        }
+    }
+}
